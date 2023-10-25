343c5524f21130f8a59d9dcb2192fd73848138b3
Marcus Matthews <mlmatthews75@gmail.com>
2023-10-24 21:49:14 -0400

    added corrective code to server.js and added app.delete to delete saved notes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -9,36 +9,49 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const dbFilePath = path.join(__dirname, 'db', 'db.json');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(__dirname + '/public/notes.html');
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(__dirname + '/db/db.json');
+    const notes = JSON.parse(fs.readFileSync(dbFilePath));
+    res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    const notes = JSON.parse(fs.readFileSync('/db/db.json'));
+    const notes = JSON.parse(fs.readFileSync(dbFilePath));
     const lastNoteId = notes.length > 0 ? notes[notes.length - 1].id : 0;
     const newNoteId = lastNoteId + 1;
     newNote.id = newNoteId;
     notes.push(newNote);
-    fs.writeFileSync('/db/db.json', JSON.stringify(notes));
+    fs.writeFileSync(dbFilePath, JSON.stringify(notes));
     res.json(newNote);
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = parseInt(req.params.id);
+    const notes = JSON.parse(fs.readFileSync(dbFilePath));
+    const noteIndex = notes.findIndex((note) => note.id === noteId);
+    if (noteIndex !== -1) {
+        notes.splice(noteIndex, 1);
+        fs.writeFileSync(dbFilePath, JSON.stringify(notes));
+        res.json({ success: true, message: 'Note deleted successfully' });
+    } else {
+        res.status(404).json({ success: false, message: 'Note not found' });
+    }
+});
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
